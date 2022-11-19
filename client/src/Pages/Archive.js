72f24f774d319d496d255775e76fc5da57a4f6e7
selinkeles <selinkeles@sabanciuniv.edu>
2022-11-19 11:53:10 +0300

    Log out user

---
+++ client/src/Pages/Archive.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Archive() {
+  return (
+    <div>
+      Archive
+    </div>
+  )
+}
+
+export default Archive
---
+++ client/src/Pages/Games.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Games() {
+  return (
+    <div>
+      Footbal Matches
+    </div>
+  )
+}
+
+export default Games
