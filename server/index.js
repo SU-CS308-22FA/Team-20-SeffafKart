58368c208fd1b50717e7e590b25f85c86cb21316
Obycoder8 <oberke@sabanciuniv.edu>
2022-12-13 23:12:37 +0300

    Minor bug fixes
    Minor bugs that occured in frontend texts and composition has been
    fixed.

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const mysql = require("mysql2");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "4125",
   database: "cruddatabase",
 });
 
